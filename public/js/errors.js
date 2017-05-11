f07c99fddedb842668ad9849d9e049e405ea6403
Sameid Usmani <sameid.usmani@gmail.com>
2017-05-10 23:06:19 -0400

    moving assets around

---
+++ public/js/errors.js
@@ -0,0 +1,22 @@
+var ERRORS = {
+    ROOM_ALREADY_EXISTS: {
+        error: 1000,
+        message: "A room with this name already exists."
+    },
+    ROOM_DOES_NOT_EXIST: {
+        error: 1001,
+        message: "A room with this name does not exist."
+    },
+    CLIENT_DISCONNECTED: {
+        error: 1002,
+        message: "The client mysteriously disconnected."
+    },
+    SERVER_DISCONNECTED: {
+        error: 1002,
+        message: "The client mysteriously disconnected."
+    }
+}
+
+if (module){
+    module.exports = ERRORS;
+}
