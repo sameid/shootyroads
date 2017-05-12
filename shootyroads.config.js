5fb44bf5b36407a3237239fea247aa1f3feb3992
Sameid Usmani <sameid.usmani@gmail.com>
2017-05-12 01:06:06 -0400

    fix

--- shootyroads.config.js
+++ shootyroads.config.js
@@ -4,10 +4,10 @@ module.exports = {
         script      : "./server.js",
         watch       : false,
         env: {
-            "NODE_ENV": "development",
+            "PORT": 3000,
         },
         env_production : {
-            "NODE_ENV": "production"
+            "PORT": 80
         }
     }]
 }
